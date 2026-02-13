84b599c310d7bbaa91659469f5c97298c7a8d02d
Tomas <tomasdi.dev@gmail.com>
2026-02-12 22:59:04 -0500

    Punto 5 con maven

---
+++ src/test/java/edu/eci/dosw/reto3/SanityTest.java
@@ -0,0 +1,12 @@
+package edu.eci.dosw.reto3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SanityTest {
+    @Test
+    void shouldRun() {
+        assertTrue(true);
+    }
+}
